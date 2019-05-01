3b50bb9a959e040fe83226f116e62704ca266d81
Matthew Gallea <mattgallea@gmail.com>
2019-05-01 16:28:09 -0400

    Update Month Convert Test
    Need to include leading 0s

--- sales_test.py
+++ sales_test.py
@@ -11,23 +11,23 @@ def test_to_usd():
 	assert d == "$1,000,000.00"
 
 def test_month_converter():
-	month = month_converter('1')
+	month = month_converter('01')
 	assert month == "January"
-	month = month_converter('2')
+	month = month_converter('02')
 	assert month == "February"
-	month = month_converter('3')
+	month = month_converter('03')
 	assert month == "March"
-	month = month_converter('4')
+	month = month_converter('04')
 	assert month == "April"
-	month = month_converter('5')
+	month = month_converter('05')
 	assert month == "May"
-	month = month_converter('6')
+	month = month_converter('06')
 	assert month == "June"
-	month = month_converter('7')
+	month = month_converter('07')
 	assert month == "July"
-	month = month_converter('8')
+	month = month_converter('08')
 	assert month == "August"
-	month = month_converter('9')
+	month = month_converter('09')
 	assert month == "September"
 	month = month_converter('10')
 	assert month == "October"
